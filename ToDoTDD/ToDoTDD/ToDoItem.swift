dc20b35962e10a892c58cda711d86f02687dd510
Elisey <eliseyozerov@outlook.com>
2022-01-04 15:38:40 +0100

    Add some tests

--- ToDoTDD/ToDoTDD/ToDoItem.swift
+++ ToDoTDD/ToDoTDD/ToDoItem.swift
@@ -5,4 +5,19 @@
 //  Created by Elisey Ozerov on 16/12/2021.
 //
 
-import Foundation
+struct ToDoItem {
+    let title: String
+    let description: String?
+    let timestamp: Double?
+    let location: Location?
+    
+    init(title: String,
+         description: String? = nil,
+         timestamp: Double? = nil,
+         location: Location? = nil) {
+        self.title = title
+        self.description = description
+        self.timestamp = timestamp
+        self.location = location
+    }
+}
--- ToDoTDD/ToDoTDDTests/ToDoItemTests.swift
+++ ToDoTDD/ToDoTDDTests/ToDoItemTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import ToDoTDD
 
 class ToDoItemTests: XCTestCase {
 
@@ -16,20 +17,27 @@ class ToDoItemTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_init_whenGivenTitle_setsTitle() {
+        let item = ToDoItem(title: "Foo")
+        XCTAssertEqual(item.title, "Foo")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_init_whenGivenDescription_setsDescription() {
+        let item = ToDoItem(title: "Foo", description: "Bar")
+        XCTAssertEqual(item.description, "Bar")
+    }
+    
+    func test_init_whenGivenTimestamp_setsTimestamp() {
+        let item = ToDoItem(title: "Foo", timestamp: 0.0)
+        XCTAssertEqual(item.timestamp, 0.0)
+    }
+    
+    func test_init_whenGivenLocation_setsLocation() {
+        let location = Location(name: "Foo")
+        let item = ToDoItem(title: "", location: location)
+        
+        XCTAssertEqual(item.location?.name, location.name)
     }
 
 }
