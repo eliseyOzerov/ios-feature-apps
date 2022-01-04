b0dc39e859b9b7db3fa22a7d2aa02540d98117bb
Elisey <eliseyozerov@outlook.com>
2022-01-04 15:37:52 +0100

    Initial commit

---
+++ ToDoTDD/ToDoTDD/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  ToDoTDD
+//
+//  Created by Elisey Ozerov on 16/12/2021.
+//
+
+import Foundation
---
+++ ToDoTDD/ToDoTDD/ToDoItem.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoItem.swift
+//  ToDoTDD
+//
+//  Created by Elisey Ozerov on 16/12/2021.
+//
+
+import Foundation
